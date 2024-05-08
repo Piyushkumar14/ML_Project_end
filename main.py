f00e9763eebc6a49e4a312d02d55b212e83025f5
Piyush Kumar <71686785+Piyushkumar14@users.noreply.github.com>
2024-05-08 12:03:11 +0530

    pipeline checked

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from src.MLProject import logger
+from src.MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom log")
+
+
+# logger.info("Welcome to our custom log")
+STAGE_NAME   = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx=================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
