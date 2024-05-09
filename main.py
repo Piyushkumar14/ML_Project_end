96d8d4cff63b28c0334ef44a626f905fc7836e95
Piyush Kumar <71686785+Piyushkumar14@users.noreply.github.com>
2024-05-09 22:58:23 +0530

    data Transformation added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.MLProject import logger
 from src.MLProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.MLProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from src.MLProject.pipeline.stage_03_data_transformation import DataTranformationTrainingPipeline
 
 # logger.info("Welcome to our custom log")
 STAGE_NAME   = "Data Ingestion stage"
@@ -19,6 +19,16 @@ STAGE_NAME   = "Data Validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx=================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME   = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTranformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\nx=================x")
 except Exception as e:
